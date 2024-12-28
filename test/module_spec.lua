8fc6017b0a55d671eb15a7eda9f9f6fb969d7265
yuys13 <yuys13@users.noreply.github.com>
2024-12-28 21:48:50 +0900

    test: add testcase

--- test/module_spec.lua
+++ test/module_spec.lua
@@ -16,7 +16,7 @@ describe('skk_get', function()
 		vim.fn.delete(tempdir, 'd')
 	end)
 
-	it('get default dics', function()
+	it('get default dicts', function()
 		local expected = {
 			'SKK-JISYO.JIS2',
 			'SKK-JISYO.geo',
@@ -42,6 +42,50 @@ describe('skk_get', function()
 			'zipcode.tar.gz',
 		}
 		skk_develop.skk_get(tempdir)
+		local actual = {}
+		for _, file in ipairs(vim.fn.glob(tempdir .. '/*', nil, true)) do
+			table.insert(actual, vim.fn.fnamemodify(file, ':t'))
+		end
+		table.sort(expected)
+		table.sort(actual)
+		assert.are_same(expected, actual)
+	end)
+
+	it('get single dict', function()
+		local expected = {
+			'SKK-JISYO.L',
+		}
+		skk_develop.skk_get(tempdir, { 'SKK-JISYO.L.gz' })
+		local actual = {}
+		for _, file in ipairs(vim.fn.glob(tempdir .. '/*', nil, true)) do
+			table.insert(actual, vim.fn.fnamemodify(file, ':t'))
+		end
+		table.sort(expected)
+		table.sort(actual)
+		assert.are_same(expected, actual)
+	end)
+
+	it('get some dicts', function()
+		local expected = {
+			'SKK-JISYO.geo',
+			'SKK-JISYO.L',
+			'SKK-JISYO.fullname',
+			'SKK-JISYO.itaiji',
+			'SKK-JISYO.jinmei',
+			'SKK-JISYO.office.zipcode',
+			'SKK-JISYO.station',
+			'SKK-JISYO.zipcode',
+			'zipcode.tar.gz',
+		}
+		skk_develop.skk_get(tempdir, {
+			'SKK-JISYO.geo.gz',
+			'SKK-JISYO.L.gz',
+			'SKK-JISYO.fullname.gz',
+			'SKK-JISYO.itaiji.gz',
+			'SKK-JISYO.jinmei.gz',
+			'SKK-JISYO.station.gz',
+			'zipcode.tar.gz',
+		})
 		local actual = {}
 		for _, file in ipairs(vim.fn.glob(tempdir .. '/*', nil, true)) do
 			table.insert(actual, vim.fn.fnamemodify(file, ':t'))
