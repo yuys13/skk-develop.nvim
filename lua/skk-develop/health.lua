c280056799618000e61f3762cda15c15fe1e1926
yuys13 <yuys13@users.noreply.github.com>
2024-12-28 21:53:34 +0900

    update: update health functions

--- lua/skk-develop/health.lua
+++ lua/skk-develop/health.lua
@@ -2,36 +2,36 @@ local M = {}
 
 M.check = function()
 	-- check plugins
-	vim.health.report_start('Checking for required plugins')
+	vim.health.start('Checking for required plugins')
 
 	if pcall(require, 'plenary') then
-		vim.health.report_ok('plenary: installed')
+		vim.health.ok('plenary: installed')
 	else
-		vim.health.report_error('plenary: not available', 'Install https://github.com/nvim-lua/plenary.nvim')
+		vim.health.error('plenary: not available', 'Install https://github.com/nvim-lua/plenary.nvim')
 	end
 
 	-- check executable
-	vim.health.report_start('Checking external dependencies')
+	vim.health.start('Checking external dependencies')
 
 	if vim.fn.executable('curl') == 1 then
-		vim.health.report_ok('curl: installed')
+		vim.health.ok('curl: installed')
 	else
-		vim.health.report_error('curl: not available')
+		vim.health.error('curl: not available')
 	end
 
 	if vim.fn.executable('tar') == 1 then
-		vim.health.report_ok('tar: installed')
+		vim.health.ok('tar: installed')
 	else
-		vim.health.report_error('tar: not available')
+		vim.health.error('tar: not available')
 	end
 
 	if vim.fn.executable('gzip') == 1 then
-		vim.health.report_ok('gzip: installed')
+		vim.health.ok('gzip: installed')
 	elseif vim.fn.executable('powershell.exe') == 1 then
-		vim.health.report_info('gzip: not available')
-		vim.health.report_ok('powershell.exe: installed')
+		vim.health.info('gzip: not available')
+		vim.health.ok('powershell.exe: installed')
 	else
-		vim.health.report_error('gzip or powershell.exe: not available')
+		vim.health.error('gzip or powershell.exe: not available')
 	end
 end
 
